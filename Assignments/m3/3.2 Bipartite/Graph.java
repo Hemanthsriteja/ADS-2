9390a11ae7ce1271032880f68dbf742a653b616a
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-11-05 22:07:49 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ Assignments/m3/3.2 Bipartite/Graph.java
@@ -0,0 +1,251 @@
+/**
+ *  The <tt>Graph</tt> class represents an undirected graph of vertices
+ *  named 0 through V-1.
+ *  It supports the following operations: add an edge to the graph,
+ *  iterate over all of the neighbors adjacent to a vertex.
+ *  Parallel edges and self-loops are permitted.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/51undirected">Section 5.1</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+class Graph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+   /**
+     * Create an empty graph with V vertices.
+     */
+    public Graph(int V) {
+        if (V < 0) throw new RuntimeException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+   /**
+     * Create a random graph with V vertices and E edges.
+     * Expected running time is proportional to V + E.
+     */
+    public Graph(int V, int E) {
+        this(V);
+        if (E < 0) throw new RuntimeException("Number of edges must be nonnegative");
+        for (int i = 0; i < E; i++) {
+            int v = (int) (Math.random() * V);
+            int w = (int) (Math.random() * V);
+            addEdge(v, w);
+        }
+    }
+
+   /**
+     * Create a digraph from input stream.
+     */
+/*    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }*/
+
+   /**
+     * Copy constructor.
+     */
+/*    public Graph(Graph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Integer> reverse = new Stack<Integer>();
+            for (int w : G.adj[v]) {
+                reverse.push(w);
+            }
+            for (int w : reverse) {
+                adj[v].add(w);
+            }
+        }
+    }*/
+
+   /**
+     * Return the number of vertices in the graph.
+     */
+    public int V() { return V; }
+
+   /**
+     * Return the number of edges in the graph.
+     */
+    public int E() { return E; }
+
+
+   /**
+     * Add the edge v-w to graph.
+     */
+    public void addEdge(int v, int w) {
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+
+   /**
+     * Return the list of neighbors of vertex v as in Iterable.
+     */
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+
+   /**
+     * Return a string representation of the graph.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        String NEWLINE = System.getProperty("line.separator");
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (int w : adj[v]) {
+                s.append(w + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+}
+
+class Bipartite {
+    private boolean isBipartite;
+    private boolean[] color;
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+
+    /**
+     * Determines whether an undirected graph is bipartite and finds either a
+     * bipartition or an odd-length cycle.
+     *
+     * @param  G the graph
+     */
+    public Bipartite(Graph G) {
+        isBipartite = true;
+        color  = new boolean[G.V()];
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+        assert check(G);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+
+            // short circuit if odd-length cycle found
+            if (cycle != null) return;
+
+            // found uncolored vertex, so recur
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                color[w] = !color[v];
+                dfs(G, w);
+            }
+
+            // if v-w create an odd-length cycle, find it
+            else if (color[w] == color[v]) {
+                isBipartite = false;
+                cycle = new Stack<Integer>();
+                cycle.push(w);  // don't need this unless you want to include start vertex twice
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+            }
+        }
+    }
+
+    /**
+     * Returns true if the graph is bipartite.
+     *
+     * @return {@code true} if the graph is bipartite; {@code false} otherwise
+     */
+    public boolean isBipartite() {
+        return isBipartite;
+    }
+
+    /**
+     * Returns the side of the bipartite that vertex {@code v} is on.
+     *
+     * @param  v the vertex
+     * @return the side of the bipartition that vertex {@code v} is on; two vertices
+     *         are in the same side of the bipartition if and only if they have the
+     *         same color
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     * @throws UnsupportedOperationException if this method is called when the graph
+     *         is not bipartite
+     */
+    public boolean color(int v) {
+        validateVertex(v);
+        if (!isBipartite)
+            throw new UnsupportedOperationException("graph is not bipartite");
+        return color[v];
+    }
+
+    /**
+     * Returns an odd-length cycle if the graph is not bipartite, and
+     * {@code null} otherwise.
+     *
+     * @return an odd-length cycle if the graph is not bipartite
+     *         (and hence has an odd-length cycle), and {@code null}
+     *         otherwise
+     */
+    public Iterable<Integer> oddCycle() {
+        return cycle;
+    }
+
+    private boolean check(Graph G) {
+        // graph is bipartite
+        if (isBipartite) {
+            for (int v = 0; v < G.V(); v++) {
+                for (int w : G.adj(v)) {
+                    if (color[v] == color[w]) {
+                        System.err.printf("edge %d-%d with %d and %d in same side of bipartition\n", v, w, v, w);
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // graph has an odd-length cycle
+        else {
+            // verify cycle
+            int first = -1, last = -1;
+            for (int v : oddCycle()) {
+                if (first == -1) first = v;
+                last = v;
+            }
+            if (first != last) {
+                System.err.printf("cycle begins with %d and ends with %d\n", first, last);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+}
